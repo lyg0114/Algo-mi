e235b8216013c8d52eb590e39d84a343e3bc2305
lyg0114 <whdnseowkd@naver.com>
2023-11-07 20:30:50 +0100

    init

---
+++ src/main/java/com/sportedu/sporteduplatform/SportEduPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sportedu.sporteduplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEduPlatformApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SportEduPlatformApplication.class, args);
+  }
+
+}
