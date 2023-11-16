be7837f89a8da1e2a7f423b58622184679a51c08
lyg0114 <whdnseowkd@naver.com>
2023-11-16 23:44:34 +0100

    등록 버튼 및 모달창 추가

--- src/main/resources/static/resources/js/main-dashboard.js
+++ src/main/resources/static/resources/js/main-dashboard.js
@@ -1,9 +1,11 @@
 function doAction() {
-  test();
+  showModal();
 }
 
-function test(){
-  $("#search").on('click', function() {
-    $("#modal-test").modal();
+function showModal() {
+  let $addQuestionBtn = $("#add-question-btn");
+  $addQuestionBtn.on('click', () => {
+    let $modalAddQuestion = $("#modal-add-question");
+    $modalAddQuestion.modal();
   });
 }
