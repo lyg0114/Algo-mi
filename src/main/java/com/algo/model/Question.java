e0ba903b453bec1e08f01a5590ec128e00c53c75
lyg0114 <whdnseowkd@naver.com>
2023-11-20 00:22:50 +0100

    Entity 추가 : UserInfo

--- src/main/java/com/algo/model/Question.java
+++ src/main/java/com/algo/model/Question.java
@@ -2,27 +2,37 @@ package com.algo.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import lombok.NoArgsConstructor;
 
 /**
  * @author : iyeong-gyo
  * @package : com.algo.model
  * @since : 18.11.23
  */
+@NoArgsConstructor
 @Entity
 @Table(name = "question")
 public class Question extends BaseEntity {
 
-  @Column(name = "title")
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "question_id")
+  private Long questionId;
+
   @NotBlank
+  @Column(name = "title")
   private String title;
 
-  @Column(name = "from_source")
   @NotBlank
+  @Column(name = "from_source")
   private String fromSource;
 
-  @Column(name = "review_count")
   @NotBlank
+  @Column(name = "review_count")
   private Integer reviewCount;
 }
---
+++ src/main/java/com/algo/model/UserInfo.java
@@ -0,0 +1,47 @@
+package com.algo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.algo.model
+ * @since : 20.11.23
+ */
+@NoArgsConstructor
+@Entity
+@Table(name = "user_info")
+public class UserInfo extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long userId;
+
+  @NotBlank
+  @Column(updatable = false, nullable = false)
+  private String email;
+
+  @NotBlank
+  @Column(name = "user_name")
+  private String userName;
+
+  @JsonIgnore
+  @Column(nullable = false)
+  private String passwd;
+
+  @OneToMany
+  @JoinColumn(name = "question_id")
+  private List<Question> questions = new ArrayList<>();
+}
