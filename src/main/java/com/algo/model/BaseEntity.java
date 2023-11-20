71f9ee3e7134780fa66851bcb3668bc551476b4c
lyg0114 <whdnseowkd@naver.com>
2023-11-20 01:18:53 +0100

    불필요코드 삭제

--- src/main/java/com/algo/model/BaseEntity.java
+++ src/main/java/com/algo/model/BaseEntity.java
@@ -1,10 +1,8 @@
 package com.algo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -12,17 +10,24 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Setter
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditEntityListener.class)
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity implements Serializable {
 
+  @CreatedDate
+  @JsonIgnore
   @Temporal(TemporalType.TIMESTAMP)
   @Column(name = "created_dt", nullable = false, updatable = false)
   private LocalDateTime createdDt;
 
+  @LastModifiedBy
+  @JsonIgnore
   @Temporal(TemporalType.TIMESTAMP)
   @Column(name = "updated_dt", nullable = false)
   private LocalDateTime updatedDt;
