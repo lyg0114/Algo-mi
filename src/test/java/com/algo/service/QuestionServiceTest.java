6f1ff186a5bb09107364681810a5fd1789bb2b70
lyg0114 <whdnseowkd@naver.com>
2023-11-27 23:02:49 +0100

    feat : Create Sample Data for test

--- src/test/java/com/algo/service/QuestionServiceTest.java
+++ src/test/java/com/algo/service/QuestionServiceTest.java
@@ -1,10 +1,15 @@
 package com.algo.service;
 
+import com.algo.model.entity.Question;
+import com.algo.model.entity.UserInfo;
+import com.algo.repository.QuestionRepository;
 import com.algo.repository.UserInfoRepository;
-import com.algo.repository.querydsl.QuestionCustomRepository;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author : iyeong-gyo
@@ -17,14 +22,42 @@ class QuestionServiceTest {
   @Autowired
   private QuestionService questionService;
   @Autowired
-  private QuestionCustomRepository questionCustomRepository;
+  private QuestionRepository questionRepository;
   @Autowired
   private UserInfoRepository userInfoRepository;
 
-  @Test
-  public void findPaginatedForQuestionsTest() {
-
+  @BeforeEach
+  void before() {
+    questionRepository.deleteAll();
+    userInfoRepository.deleteAll();
   }
 
+  @Transactional
+  @Test
+  public void findPaginatedForQuestionsTest() {
+    SampleData.createSamplefindPaginatedForQuestionsTest(questionRepository, userInfoRepository);
+  }
 
+  static class SampleData {
+    public static void createSamplefindPaginatedForQuestionsTest(
+        QuestionRepository questionRepository, UserInfoRepository userInfoRepository
+    ) {
+      userInfoRepository.saveAll(List.of(
+          UserInfo.builder().userName("user-1").email("user-1@example.com").passwd("passowrd-1").build(),
+          UserInfo.builder().userName("user-2").email("user-2@example.com").passwd("passowrd-2").build())
+      );
+      UserInfo user1 = userInfoRepository.findById(1L).get();
+      UserInfo user2 = userInfoRepository.findById(2L).get();
+      questionRepository.saveAll(
+          List.of(
+              Question.builder().title("titld-1").url("http://localhost/leetcode/url").fromSource("leetCode").reviewCount(0).userInfo(user1).build(),
+              Question.builder().title("titld-2").url("http://localhost/leetcode/url").fromSource("leetCode").reviewCount(0).userInfo(user1).build(),
+              Question.builder().title("titld-3").url("http://localhost/leetcode/url").fromSource("leetCode").reviewCount(0).userInfo(user1).build(),
+              Question.builder().title("titld-4").url("http://localhost/leetcode/url").fromSource("leetCode").reviewCount(0).userInfo(user2).build(),
+              Question.builder().title("titld-5").url("http://localhost/leetcode/url").fromSource("leetCode").reviewCount(0).userInfo(user2).build(),
+              Question.builder().title("titld-6").url("http://localhost/leetcode/url").fromSource("leetCode").reviewCount(0).userInfo(user2).build()
+          )
+      );
+    }
+  }
 }
