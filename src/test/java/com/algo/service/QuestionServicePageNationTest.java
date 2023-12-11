ad19aab3e0342a78f7ab6a987c900e9544d537de
lyg0114 <whdnseowkd@naver.com>
2023-12-11 17:57:07 +0100

    pagenation_first_page_test

--- src/test/java/com/algo/service/QuestionServicePageNationTest.java
+++ src/test/java/com/algo/service/QuestionServicePageNationTest.java
@@ -1,10 +1,12 @@
 package com.algo.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,11 +26,8 @@ class QuestionServicePageNationTest {
   private MockMvc mockMvc;
 
   @Test
-  public void pagenation_attribuet_should_exist() throws Exception {
-    mockMvc.perform(
-            get("/customer/main-dashboard?page=2&size=5")
-                .param("title", "DFS-Question")
-        )
+  public void pagenation_attribute_should_exist() throws Exception {
+    mockMvc.perform(get("/customer/main-dashboard"))
         .andExpect(status().isOk())
         .andExpect(model().attributeExists("contents"))
         .andExpect(model().attributeExists("totalPages"))
@@ -40,5 +39,35 @@ class QuestionServicePageNationTest {
         .andExpect(model().attributeExists("hasNext"))
         .andExpect(model().attributeExists("currentPage"))
         .andExpect(view().name("customer/main-dashboard"));
+  }
+
+  @Test
+  public void pagenation_first_page_test() throws Exception {
+    mockMvc.perform(get("/customer/main-dashboard?page=0&size=10"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("customer/main-dashboard"))
+        .andDo(result -> {
+          Map<String, Object> model = result.getModelAndView().getModel();
+          assertThat(model.get("contents")).isNotNull();
+          assertThat(model.get("totalPages")).isEqualTo(15);
+          assertThat(model.get("isStartNumberPeriod")).isEqualTo(true);
+          assertThat(model.get("isEndNumberPeriod")).isEqualTo(false);
+          assertThat(model.get("startNumber")).isEqualTo(1);
+          assertThat(model.get("endNumber")).isEqualTo(10);
+          assertThat(model.get("hasPrevious")).isEqualTo(false);
+          assertThat(model.get("hasNext")).isEqualTo(true);
+          assertThat(model.get("currentPage")).isEqualTo(0);
+        });
+  }
+
+  private static void printPageNationModel(Map<String, Object> model) {
+    System.out.println("totalPages: " + model.get("totalPages"));
+    System.out.println("isStartNumberPeriod: " + model.get("isStartNumberPeriod"));
+    System.out.println("isEndNumberPeriod: " + model.get("isEndNumberPeriod"));
+    System.out.println("startNumber: " + model.get("startNumber"));
+    System.out.println("endNumber: " + model.get("endNumber"));
+    System.out.println("hasPrevious: " + model.get("hasPrevious"));
+    System.out.println("hasNext: " + model.get("hasNext"));
+    System.out.println("currentPage: " + model.get("currentPage"));
   }
 }
