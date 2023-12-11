c181ce7072ae8f771b06cdfe9de86c08eedadd33
lyg0114 <whdnseowkd@naver.com>
2023-12-11 17:26:53 +0100

    feat : Add QuestionServicePageNationTest pagenation_attribuet_should_exist

--- src/test/java/com/algo/service/QuestionServicePageNationTest.java
+++ src/test/java/com/algo/service/QuestionServicePageNationTest.java
@@ -1,27 +1,44 @@
 package com.algo.service;
 
-import com.algo.repository.QuestionRepository;
-import com.algo.repository.UserInfoRepository;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * @author : iyeong-gyo
  * @package : com.algo.service
  * @since : 27.11.23
  */
-@Transactional
 @SpringBootTest
+@AutoConfigureMockMvc
 class QuestionServicePageNationTest {
 
-  @Autowired private QuestionService questionService;
-  @Autowired protected QuestionRepository questionRepository;
-  @Autowired protected UserInfoRepository userInfoRepository;
+  @Autowired
+  private MockMvc mockMvc;
 
   @Test
-  public void pagenation_test(){
-
+  public void pagenation_attribuet_should_exist() throws Exception {
+    mockMvc.perform(
+            get("/customer/main-dashboard?page=2&size=5")
+                .param("title", "DFS-Question")
+        )
+        .andExpect(status().isOk())
+        .andExpect(model().attributeExists("contents"))
+        .andExpect(model().attributeExists("totalPages"))
+        .andExpect(model().attributeExists("isStartNumberPeriod"))
+        .andExpect(model().attributeExists("isEndNumberPeriod"))
+        .andExpect(model().attributeExists("startNumber"))
+        .andExpect(model().attributeExists("endNumber"))
+        .andExpect(model().attributeExists("hasPrevious"))
+        .andExpect(model().attributeExists("hasNext"))
+        .andExpect(model().attributeExists("currentPage"))
+        .andExpect(view().name("customer/main-dashboard"));
   }
 }
