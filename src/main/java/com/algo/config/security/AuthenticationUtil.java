5a998f0465d1ce4aa469b8534aa90ecc98a26424
lyg0114 <whdnseowkd@naver.com>
2024-01-03 22:22:38 +0100

    feat : security with jwt + springSecurity

--- src/main/java/com/algo/config/security/AuthenticationUtil.java
+++ src/main/java/com/algo/config/security/AuthenticationUtil.java
@@ -1,8 +1,5 @@
 package com.algo.config.security;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
 /**
  * @author : iyeong-gyo
  * @package : com.algo.config.security
@@ -11,7 +8,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class AuthenticationUtil {
 
   public static String getUserName() {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    return authentication.getName();
+    return null;
   }
 }
--- src/main/java/com/algo/controller/rest/QuestionRestController.java
+++ src/main/java/com/algo/controller/rest/QuestionRestController.java
@@ -9,7 +9,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
