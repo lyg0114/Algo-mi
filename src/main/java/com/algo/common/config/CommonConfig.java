86e55ec67f4f4bd9b63dc19966b44826f72dc336
lyg0114 <whdnseowkd@naver.com>
2024-02-10 08:24:49 +0100

    feat : change sample data

--- src/main/java/com/algo/common/config/CommonConfig.java
+++ src/main/java/com/algo/common/config/CommonConfig.java
@@ -5,6 +5,7 @@ import com.algo.auth.domain.UserInfoRepository;
 import com.algo.question.domain.Question;
 import com.algo.question.domain.QuestionRepository;
 import java.util.List;
+import java.util.Random;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -35,31 +36,38 @@ public class CommonConfig {
   @Profile("local")
   @Bean
   public CommandLineRunner initData(
-      UserInfoRepository userInfoRepository,
-      QuestionRepository questionRepository,
-      PasswordEncoder passwordEncoder
+      UserInfoRepository userInfoRepository, QuestionRepository questionRepository,
+      PasswordEncoder encoder
   ) {
     return args -> {
       userInfoRepository.saveAll(
           List.of(
-              UserInfo.builder().userId(1L).userName("kyle").email("user@example.com").passwd(passwordEncoder.encode("password")).role("USER").build(),
-              UserInfo.builder().userId(2L).userName("jhone").email("jhone@example.com").passwd(passwordEncoder.encode("password")).role("GUEST").build(),
-              UserInfo.builder().userId(3L).userName("lizzy").email("lizzy@example.com").passwd(passwordEncoder.encode("password")).role("USER").build(),
-              UserInfo.builder().userId(4L).userName("tom").email("tom@example.com").passwd(passwordEncoder.encode("password")).role("USER").build()
+              UserInfo.builder().userId(1L).userName("kyle").email("user@example.com")
+                  .passwd(encoder.encode("password")).role("USER").build(),
+              UserInfo.builder().userId(2L).userName("jhone").email("jhone@example.com")
+                  .passwd(encoder.encode("password")).role("GUEST").build(),
+              UserInfo.builder().userId(3L).userName("lizzy").email("lizzy@example.com")
+                  .passwd(encoder.encode("password")).role("USER").build(),
+              UserInfo.builder().userId(4L).userName("tom").email("tom@example.com")
+                  .passwd(encoder.encode("password")).role("USER").build()
           )
       );
 
+      Random random = new Random();
+      List<String> fromSources
+          = List.of("LEETCODE", "HACKERRANK", "CODILITY", "BAEKJOON", "PROGRAMMERS");
+      List<String> questionTypes = List.of("GREEDY", "DFS", "BFS");
 
-      for (int i = 0; i < 20; i++) {
+      for (int i = 0; i < 50; i++) {
         questionRepository.save(
             Question.builder()
                 .title("title-" + i)
                 .url("http://sample-url/" + i)
                 .reviewCount(3)
-                .fromSource("LEETCODE")
-                .questionType("GREEDY")
+                .fromSource(fromSources.get(random.nextInt(fromSources.size())))
+                .questionType(questionTypes.get(random.nextInt(questionTypes.size())))
                 .userInfo(UserInfo.builder().userId(1L).userName("kyle").email("user@example.com")
-                    .passwd(passwordEncoder.encode("password")).role("ROLE_USER").build())
+                    .passwd(encoder.encode("password")).role("ROLE_USER").build())
                 .build()
         );
       }
