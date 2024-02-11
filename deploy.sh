8bf421caee5822f2fe76234e808894001cc40c48
lyg0114 <whdnseowkd@naver.com>
2024-02-11 10:43:24 +0100

    style : update style

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ IMAGE_NAME="algo-mi-api"
 TAG_VERSION="latest-v4" # should change version info
 
 ./gradlew clean build
+docker push whdnseowkd/${IMAGE_NAME}:${TAG_VERSION}
+
 docker build -t ${IMAGE_NAME} .
 docker run -p 3000:3000 ${IMAGE_NAME}
--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ IMAGE_NAME="algo-mi-api"
 TAG_VERSION="latest-v4" # should change version info
 
 ./gradlew clean build
+
 docker build -t ${IMAGE_NAME} .
 docker tag ${IMAGE_NAME}:latest whdnseowkd/${IMAGE_NAME}:${TAG_VERSION}
 docker push whdnseowkd/${IMAGE_NAME}:${TAG_VERSION}
