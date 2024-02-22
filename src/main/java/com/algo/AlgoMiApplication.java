2bdefd0a4957aa598760a7791f771e5bea9914cc
lyg0114 <whdnseowkd@naver.com>
2024-02-22 11:59:57 +0100

    style : refactoring

--- src/main/java/com/algo/AlgoMiApplication.java
+++ src/main/java/com/algo/AlgoMiApplication.java
@@ -1,8 +1,18 @@
 package com.algo;
 
+import com.algo.auth.domain.UserInfo;
+import com.algo.auth.domain.UserInfoRepository;
+import com.algo.question.domain.Question;
+import com.algo.question.domain.QuestionRepository;
+import java.util.List;
+import java.util.Random;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +20,48 @@ public class AlgoMiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AlgoMiApplication.class, args);
+  }
+
+  // 샘플데이터로 구동시 주석 해제
+  @Profile("local")
+  @Bean
+  public CommandLineRunner initData(
+      UserInfoRepository userInfoRepository, QuestionRepository questionRepository,
+      PasswordEncoder encoder
+  ) {
+    return args -> {
+      userInfoRepository.saveAll(
+          List.of(
+              UserInfo.builder().userId(1L).userName("kyle").email("user@example.com")
+                  .passwd(encoder.encode("password")).role("USER").build(),
+              UserInfo.builder().userId(2L).userName("jhone").email("jhone@example.com")
+                  .passwd(encoder.encode("password")).role("GUEST").build(),
+              UserInfo.builder().userId(3L).userName("lizzy").email("lizzy@example.com")
+                  .passwd(encoder.encode("password")).role("USER").build(),
+              UserInfo.builder().userId(4L).userName("tom").email("tom@example.com")
+                  .passwd(encoder.encode("password")).role("USER").build()
+          )
+      );
+
+      Random random = new Random();
+      List<String> fromSources
+          = List.of("LEETCODE", "HACKERRANK", "CODILITY", "BAEKJOON", "PROGRAMMERS");
+      List<String> questionTypes = List.of("GREEDY", "DFS", "BFS");
+
+      for (int i = 0; i < 50; i++) {
+        questionRepository.save(
+            Question.builder()
+                .title("title-" + i)
+                .url("http://sample-url/" + i)
+                .reviewCount(3)
+                .fromSource(fromSources.get(random.nextInt(fromSources.size())))
+                .questionType(questionTypes.get(random.nextInt(questionTypes.size())))
+                .userInfo(UserInfo.builder().userId(1L).userName("kyle").email("user@example.com")
+                    .passwd(encoder.encode("password")).role("ROLE_USER").build())
+                .build()
+        );
+      }
+    };
   }
 }
 
--- src/main/java/com/algo/common/config/CommonConfig.java
+++ src/main/java/com/algo/common/config/CommonConfig.java
@@ -1,17 +1,10 @@
 package com.algo.common.config;
 
-import com.algo.auth.domain.UserInfo;
-import com.algo.auth.domain.UserInfoRepository;
-import com.algo.question.domain.Question;
-import com.algo.question.domain.QuestionRepository;
-import java.util.List;
-import java.util.Random;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -22,6 +15,9 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CommonConfig {
 
+  @Autowired
+  private RestTemplateBuilder builder;
+
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
@@ -29,48 +25,6 @@ public class CommonConfig {
 
   @Bean
   public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
-
-  // 샘플데이터로 구동시 주석 해제
-  @Profile("local")
-  @Bean
-  public CommandLineRunner initData(
-      UserInfoRepository userInfoRepository, QuestionRepository questionRepository,
-      PasswordEncoder encoder
-  ) {
-    return args -> {
-      userInfoRepository.saveAll(
-          List.of(
-              UserInfo.builder().userId(1L).userName("kyle").email("user@example.com")
-                  .passwd(encoder.encode("password")).role("USER").build(),
-              UserInfo.builder().userId(2L).userName("jhone").email("jhone@example.com")
-                  .passwd(encoder.encode("password")).role("GUEST").build(),
-              UserInfo.builder().userId(3L).userName("lizzy").email("lizzy@example.com")
-                  .passwd(encoder.encode("password")).role("USER").build(),
-              UserInfo.builder().userId(4L).userName("tom").email("tom@example.com")
-                  .passwd(encoder.encode("password")).role("USER").build()
-          )
-      );
-
-      Random random = new Random();
-      List<String> fromSources
-          = List.of("LEETCODE", "HACKERRANK", "CODILITY", "BAEKJOON", "PROGRAMMERS");
-      List<String> questionTypes = List.of("GREEDY", "DFS", "BFS");
-
-      for (int i = 0; i < 50; i++) {
-        questionRepository.save(
-            Question.builder()
-                .title("title-" + i)
-                .url("http://sample-url/" + i)
-                .reviewCount(3)
-                .fromSource(fromSources.get(random.nextInt(fromSources.size())))
-                .questionType(questionTypes.get(random.nextInt(questionTypes.size())))
-                .userInfo(UserInfo.builder().userId(1L).userName("kyle").email("user@example.com")
-                    .passwd(encoder.encode("password")).role("ROLE_USER").build())
-                .build()
-        );
-      }
-    };
+    return builder.build();
   }
 }
