c42bd4a6638523b0d882c7448fb0091cede01fbb
lyg0114 <whdnseowkd@naver.com>
2024-03-27 23:13:37 +0100

    refactor : 반환값 수정

--- src/main/java/com/algo/auth/ui/AuthController.java
+++ src/main/java/com/algo/auth/ui/AuthController.java
@@ -138,6 +138,6 @@ public class AuthController {
     emailCheckRepository.save(emailCheck);
 
     return ResponseEntity
-        .ok(new SignUpResponse(userInfo.getEmail(), "회원가입이 완료되었습니다."));
+        .ok(new SignUpResponse(userInfo.getEmail(), "회원가입이 완료되었습니다 로그인해 주세요."));
   }
 }
