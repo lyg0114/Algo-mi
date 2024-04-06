06adbd23d113b11096e7d52842bf9ec86580d385
lyg0114 <whdnseowkd@naver.com>
2024-04-06 11:49:23 +0200

    fix : 이메일 인증 안내문구 css 개선, 이메일 전송 실패했을경우 예외처리

--- src/main/java/com/algo/alert/application/EmailServiceImpl.java
+++ src/main/java/com/algo/alert/application/EmailServiceImpl.java
@@ -1,11 +1,13 @@
 package com.algo.alert.application;
 
 import com.algo.auth.domain.CheckEmail;
+import com.algo.exception.custom.SignUpFailException;
 import com.algo.recovery.application.RecoveryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,7 +18,7 @@ import org.springframework.stereotype.Service;
  * @package : com.algo.email.application
  * @since : 12.03.24
  */
-@Profile({"local", "prod", "dev" ,"localprod"})
+@Profile({"local", "prod", "dev", "localprod"})
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -37,32 +39,35 @@ public class EmailServiceImpl implements EmailService {
   @Scheduled(cron = "${cron.rule}", zone = "${time.zone}")
   public void sendSimpleMessage() {
     String title = recoveryService.createTitle();
-    String text = recoveryService.createText();
+    String content = recoveryService.createText();
     message.setFrom(from);
     message.setTo(to); //TODO : 설정파일을 읽어 가져오는 to 정보를 DB에서 불러와서 사용자별로 전송되도록 해야함.
     message.setSubject(title);
-    message.setText(text);
+    message.setText(content);
     emailSender.send(message);
   }
 
   @Override
   public void sendSignUpEamil(CheckEmail checkEmail) {
-    String confirmUrl = host + "/check-email?token=" + checkEmail.getCheckId();
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setFrom(from);
-    String email = checkEmail.getUserInfo().getEmail();
-    message.setTo(email);
-    message.setSubject("[AGO-MI 회원가입 인증]");
-
-    //TODO : 이메일 내용 CSS 개선 필요
-    String htmlContent = "<html><body>" +
-        "<p>아래의 링크를 클릭하여 인증을 완료하여 주세요.</p>" +
-        "<p><a href=\"" + confirmUrl + "\">인증 링크</a></p>" +
-        "<p>이 링크는 15분 동안 유효합니다. 만약 인증을 완료하지 못하셨다면 다시 시도해주세요.</p>" +
-        "<p>감사합니다.</p>" +
-        "</body></html>";
-
-    message.setText(htmlContent);
-    emailSender.send(message);
+    try {
+      String confirmUrl = host + "/check-email?token=" + checkEmail.getCheckId();
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setFrom(from);
+      String email = checkEmail.getUserInfo().getEmail();
+      message.setTo(email);
+      message.setSubject("[AGO-MI 회원가입 인증]");
+      String htmlContent =
+          "[AGO-MI 회원가입 인증] \n" +
+              "아래의 링크를 클릭하여 본인 인증을 완료하여 주세요.\n" +
+              confirmUrl + "\n" +
+              "이 링크는 15분 동안 유효합니다. \n" +
+              "감사합니다.\n";
+      message.setText(htmlContent);
+      emailSender.send(message);
+    } catch (MailException ex) {
+      log.error("error : Email transmission failed.");
+      log.error(ex.getMessage());
+      throw new SignUpFailException("메일 전송에 실패하였습니다. 관리자에게 문의하여 주세요");
+    }
   }
 }
---
+++ src/main/java/com/algo/exception/custom/SignUpFailException.java
@@ -0,0 +1,13 @@
+package com.algo.exception.custom;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.algo.exception.custom
+ * @since : 06.04.24
+ */
+public class SignUpFailException extends RuntimeException {
+
+  public SignUpFailException(String message) {
+    super(message);
+  }
+}
