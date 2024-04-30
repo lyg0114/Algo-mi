d7304ec2e7b5bba976aa8ee5bdec9b1fff418e36
lyg0114 <whdnseowkd@naver.com>
2024-04-30 14:15:06 +0200

    feat : 유효성 처리 로직 추가

--- src/main/java/com/algo/profile/application/ProfileService.java
+++ src/main/java/com/algo/profile/application/ProfileService.java
@@ -1,5 +1,7 @@
 package com.algo.profile.application;
 
+import java.util.Objects;
+import org.springframework.http.MediaType;
 import com.algo.auth.domain.UserInfo;
 import com.algo.auth.domain.UserInfoRepository;
 import com.algo.auth.infrastructure.AuthenticationUtil;
@@ -30,6 +32,9 @@ public class ProfileService {
   //TODO : 썸네일 파일 저장시 파일 사이즈 작게 처리
   @Transactional
   public void storeProfile(MultipartFile file) {
+    if (file == null) { throw new IllegalArgumentException("Failed to store empty file."); }
+    if (!isImageFile(file)) { throw new IllegalArgumentException("this is not image file"); }
+
     // 썸네일 파일 저장
     FileDetail fileDetail = storageService.store(file);
 
@@ -39,8 +44,14 @@ public class ProfileService {
     userInfoRepository.save(userInfo);
   }
 
+  private boolean isImageFile(MultipartFile file) {
+    MediaType fileType = MediaType.parseMediaType(Objects.requireNonNull(file.getContentType()));
+    return fileType.getType().equals("image");  }
+
   public Resource getImage() {
-    UserInfo userInfo = userInfoRepository.findUserInfoByEmailAndIsActivateTrue(authenticationUtil.getEmail()).orElseThrow();
+    UserInfo userInfo = userInfoRepository
+        .findUserInfoByEmailAndIsActivateTrue(authenticationUtil.getEmail())
+        .orElseThrow();
     Long fileId = userInfo.getProfile().getFileId();
     return storageService.loadAsResource(fileId);
   }
