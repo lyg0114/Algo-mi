b84ab7403c0bc8d1d2400bb4fceb1c96e9436fe6
lyg0114 <whdnseowkd@naver.com>
2024-04-30 22:31:22 +0200

    feat : 배포스크립트 수정

--- src/main/java/com/algo/storage/StorageProperties.java
+++ src/main/java/com/algo/storage/StorageProperties.java
@@ -5,7 +5,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
-  public static String LOCATION = "src/main/resources/static/images";
+  // 실제물리환경에서
+//  public static String LOCATION = "src/main/resources/static/images";
+
+  // 도커환경에서
+  public static String LOCATION = "/path/to/upload/directory";
 
   public String getLocation() {
     return LOCATION;
